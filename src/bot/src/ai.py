7848042bb3b798eb8e88d7281a86c499a27dc316
Vadim Rutkovsky <vrutkovs@redhat.com>
2025-05-16 14:53:17 +0200

    bot: tweak params

--- src/bot/src/ai.py
+++ src/bot/src/ai.py
@@ -43,8 +43,8 @@ async def img2img_pipeline(image: Image.Image, prompt: str) -> bytes:
         num_inference_steps=20,
         width=width,
         height=height,
-        guidance_scale=3.0,
-        strength=0.5,
+        guidance_scale=5.0,
+        strength=0.8,
         scheduler="UniPC",
     )
     infer_request_json = json.dumps(
