a971da2f309b4fa3060b4c1747097bd01c3adce4
Vadim Rutkovsky <vrutkovs@redhat.com>
2025-05-17 09:09:25 +0200

    Update ai.py

--- src/bot/src/ai.py
+++ src/bot/src/ai.py
@@ -43,10 +43,10 @@ async def img2img_pipeline(image: Image.Image, prompt: str) -> bytes:
         num_inference_steps=20,
         width=width,
         height=height,
-        guidance_scale=3.0,
-        strength=0.8,
+        guidance_scale=1.0,
+        strength=0.5,
         scheduler="DPM++ 2M",
-        cross_attention=0.5,
+        cross_attention=0.7,
     )
     infer_request_json = json.dumps(
         {"instances": [infer_request]},
